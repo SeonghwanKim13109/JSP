45716c9798db9b8f2f3001f66d1493fad51c2de5
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-11 18:44:21 +0900

    database 접근을 위한 dao dto

---
+++ jsp_daotoex/src/com/java/jsp/MemberDTO.java
@@ -0,0 +1,76 @@
+package com.java.jsp;
+
+public class MemberDTO {
+	private String name;
+	private String birth;
+	private String id;
+	private String pw;
+	private String phone1;
+	private String phone2;
+	private String phone3;
+	private String sex;
+	
+	public MemberDTO(String name, String birth, String id, String pw, String phone1, String phone2, String phone3,String sex) {
+		super();
+		this.name = name;
+		this.birth = birth;
+		this.id = id;
+		this.pw = pw;
+		this.phone1 = phone1;
+		this.phone2 = phone2;
+		this.phone3 = phone3;
+		this.sex = sex;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getPhone1() {
+		return phone1;
+	}
+	public void setPhone1(String phone1) {
+		this.phone1 = phone1;
+	}
+	public String getPhone2() {
+		return phone2;
+	}
+	public void setPhone2(String phone2) {
+		this.phone2 = phone2;
+	}
+	public String getPhone3() {
+		return phone3;
+	}
+	public void setPhone3(String phone3) {
+		this.phone3 = phone3;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	
+}
